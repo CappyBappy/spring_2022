b770c2cd9c201bc537960b4667b34157bb84fdcf
catherinelarson <catherinelarson456@gmail.com>
2022-01-05 15:07:51 -0600

    learned about packages and played around with some

---
+++ packages/pack1.java
@@ -0,0 +1,9 @@
+package firstPack;
+public class pack1 {
+  public static void main(String[] args) {
+    System.out.println("This is pack1");
+  }
+  public void test() {
+    System.out.println("works");
+  }
+}
---
+++ packages/packUse.java
@@ -0,0 +1,7 @@
+import firstPack.pack1;
+class packUse {
+  public static void main(String[] args) {
+    pack1 test = new pack1();
+    test.test();
+  }
+}
